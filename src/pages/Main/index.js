ad2466bddda77022613de8370e72e033046d1f56
luthfynur <nurluthfy98@gmail.com>
2022-03-16 00:21:15 +0700

    add onClick to get new advise

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -6,6 +6,7 @@ import { useEffect, useState } from 'react';
 
 export default function Main() {
   const [advice, setAdvice] = useState({});
+
   function getAdvice() {
     axios
       .get('https://api.adviceslip.com/advice')
@@ -26,7 +27,7 @@ export default function Main() {
   }, []);
 
   return (
-    <div class="flex h-screen flex-col justify-center items-center bg-[#1f2632]">
+    <div class="animate-pulse flex h-screen flex-col justify-center items-center bg-[#1f2632]">
       <div class="relative rounded-xl bg-[#323a49] mt-auto mb-auto p-4 w-[320px] sm:w-[550px] sm:p-6">
         <p class="font-manrope text-[14px] text-[#52ffa8] text-center tracking-[4px] p-8">
           ADVICE {advice.id}
@@ -35,12 +36,16 @@ export default function Main() {
           "{advice.advice}"
         </p>
         <Divider />
-        <div class="absolute -bottom-[20px] sm:left-[253px] left-[140px]">
+        <div
+          class="absolute -bottom-[20px] sm:left-[253px] left-[140px]"
+          onClick={getAdvice}
+        >
           <div class="bg-[#52ffa8] w-[50px] h-[50px] rounded-full p-[12.48px] hover:shadow-[-0.2px_2px_10px_10px_rgba(82,255,168,0.4)]">
             <DiceIcon />
           </div>
         </div>
       </div>
+
       <Footer />
     </div>
   );
